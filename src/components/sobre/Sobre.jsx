36c31c6c79acfa777cfd31a25ba0b248ff47ac4a
Murillo <murillohsantos@gmail.com>
2025-01-08 23:02:36 -0300

    alterando idade no sobre

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -11,7 +11,7 @@ export default function Sobre() {
 
       <div className='div-text-sobre'>
         <h1>SOBRE MIM</h1>
-        <p>Sou um desenvolvedor front-end com 21 anos de idade e um ano de experiência como freelancer. <span id='mobile-off'>Desde que entrei no mundo da tecnologia, descobri um universo fascinante e desafiador que me permitiu mergulhar de cabeça na criação de aplicativos web dinâmicos e inovadores.</span></p>
+        <p>Sou um desenvolvedor front-end com 22 anos de idade e um ano de experiência como freelancer. <span id='mobile-off'>Desde que entrei no mundo da tecnologia, descobri um universo fascinante e desafiador que me permitiu mergulhar de cabeça na criação de aplicativos web dinâmicos e inovadores.</span></p>
         <p>Possuo conhecimento em linguagens front-end como HTML, CSS e JavaScript, além de estar familiarizado com bibliotecas e ferramentas essenciais como Jest e React Testing Library para testes automatizados e garantia de qualidade em meus projetos. Além disso, também tenho experiência no uso do MySQL.</p>
         <p>Durante meu tempo como freelancer, tive o privilégio de colaborar em diversos projetos que expandiram meu conjunto de habilidades e conhecimentos.</p>
       </div>
