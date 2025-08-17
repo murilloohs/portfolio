7e2563a99782dada098b2f81df6b3b3eace74625
Murillo <murillohsantos@gmail.com>
2025-08-17 14:06:34 -0300

    removendo alguns comentarios

--- src/components/stacks/Stacks.jsx
+++ src/components/stacks/Stacks.jsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
@@ -25,52 +24,6 @@ const stacksData = [
   { src: Java, alt: 'Logo Java', caption: 'Java' },
   { src: Mysql, alt: 'Logo MySQL', caption: 'MySQL' },
 ];
-
-// const Stacks = () => {
-//   const navigationPrevRef = useRef(null);
-//   const navigationNextRef = useRef(null);
-
-//   return (
-//     <div className='div-stacks'>
-//       <div className='wrapper-stacks'>
-//         <div ref={navigationPrevRef} className="swiper-button-prev"></div>
-        
-//         <Swiper
-//           modules={[Navigation, Pagination]}
-//           onInit={(swiper) => {
-//             swiper.params.navigation.prevEl = navigationPrevRef.current;
-//             swiper.params.navigation.nextEl = navigationNextRef.current;
-//             swiper.navigation.init();
-//             swiper.navigation.update();
-//           }}
-//           pagination={{ clickable: true }}
-//           loop={false}
-//           spaceBetween={25}
-//           slidesPerView={3}
-//           breakpoints={{
-//             0: { slidesPerView: 1, spaceBetween: 20 },
-//             768: { slidesPerView: 2, spaceBetween: 25 },
-//             1024: { slidesPerView: 3, spaceBetween: 25 },
-//           }}
-//           className="mySwiper"
-//         >
-//           {stacksData.map((stack, index) => (
-//             <SwiperSlide key={index}>
-//               <figure className="stack-figure">
-//                 <img src={stack.src} alt={stack.alt} />
-//                 <figcaption className="descricao">{stack.caption}</figcaption>
-//               </figure>
-//             </SwiperSlide>
-//           ))}
-//         </Swiper>
-        
-//         <div ref={navigationNextRef} className="swiper-button-next"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Stacks;
 
 const Stacks = () => {
   return (
