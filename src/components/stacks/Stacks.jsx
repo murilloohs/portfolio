2ae82d2d621c4a5d7a1b39799dcfeb53d8d8458e
Murillo <murillohsantos@gmail.com>
2025-08-17 14:04:08 -0300

    teste fix bug seta carrossel

--- src/components/stacks/Stacks.jsx
+++ src/components/stacks/Stacks.jsx
@@ -26,31 +26,76 @@ const stacksData = [
   { src: Mysql, alt: 'Logo MySQL', caption: 'MySQL' },
 ];
 
-const Stacks = () => {
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
+// const Stacks = () => {
+//   const navigationPrevRef = useRef(null);
+//   const navigationNextRef = useRef(null);
 
+//   return (
+//     <div className='div-stacks'>
+//       <div className='wrapper-stacks'>
+//         <div ref={navigationPrevRef} className="swiper-button-prev"></div>
+        
+//         <Swiper
+//           modules={[Navigation, Pagination]}
+//           onInit={(swiper) => {
+//             swiper.params.navigation.prevEl = navigationPrevRef.current;
+//             swiper.params.navigation.nextEl = navigationNextRef.current;
+//             swiper.navigation.init();
+//             swiper.navigation.update();
+//           }}
+//           pagination={{ clickable: true }}
+//           loop={false}
+//           spaceBetween={25}
+//           slidesPerView={3}
+//           breakpoints={{
+//             0: { slidesPerView: 1, spaceBetween: 20 },
+//             768: { slidesPerView: 2, spaceBetween: 25 },
+//             1024: { slidesPerView: 3, spaceBetween: 25 },
+//           }}
+//           className="mySwiper"
+//         >
+//           {stacksData.map((stack, index) => (
+//             <SwiperSlide key={index}>
+//               <figure className="stack-figure">
+//                 <img src={stack.src} alt={stack.alt} />
+//                 <figcaption className="descricao">{stack.caption}</figcaption>
+//               </figure>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+        
+//         <div ref={navigationNextRef} className="swiper-button-next"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Stacks;
+
+const Stacks = () => {
   return (
     <div className='div-stacks'>
       <div className='wrapper-stacks'>
-        <div ref={navigationPrevRef} className="swiper-button-prev"></div>
-        
         <Swiper
           modules={[Navigation, Pagination]}
-          onInit={(swiper) => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-            swiper.navigation.init();
-            swiper.navigation.update();
-          }}
+          navigation
           pagination={{ clickable: true }}
           loop={false}
           spaceBetween={25}
           slidesPerView={3}
           breakpoints={{
-            0: { slidesPerView: 1, spaceBetween: 20 },
-            768: { slidesPerView: 2, spaceBetween: 25 },
-            1024: { slidesPerView: 3, spaceBetween: 25 },
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 25
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 25
+            },
           }}
           className="mySwiper"
         >
@@ -63,8 +108,6 @@ const Stacks = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        
-        <div ref={navigationNextRef} className="swiper-button-next"></div>
       </div>
     </div>
   );
